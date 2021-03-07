b1673ad5f8bca1f05ed10fccf8a933130e46ad43
michal097 <slusarczyk.michal23@gmail.com>
2021-03-07 17:50:56 +0100

    remove issueCounter

--- src/main/java/com/example/demo/mongoRepo/IssueCounterRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.mongoRepo;
-
-import com.example.demo.model.IssueCounter;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface IssueCounterRepo extends MongoRepository<IssueCounter, String> {
-}
